264eb613364f86105f83423ee909a1b0d0e7a7d3
191130620@qq.com <h191130620>
2018-09-11 10:17:34 +0800

    调试代码运行

---
+++ tgcloud-modules/tgcloud-system-admin/src/main/java/com/tiger/tgcloud/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.tiger.tgcloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @description:
+ * @author: tiger
+ * @date: 2018/9/6 21:21
+ * @version: V1.0
+ * @modified by:
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Configure.
+     *
+     * @param http the http
+     * @throws Exception the exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 禁用csrf
+        http.csrf().disable();
+
+    }
+
+}
