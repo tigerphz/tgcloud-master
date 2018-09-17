c3fb6291da539c6f3369b6247f8fc432143846ce
191130620@qq.com <h191130620>
2018-09-17 22:35:17 +0800

    调试代码运行

---
+++ tgcloud-gateway-zuul/src/main/java/com/tiger/tgcloud/gateway/TgcloudGatewayZuulApplication.java
@@ -0,0 +1,57 @@
+package com.tiger.tgcloud.gateway;
+
+import com.didispace.swagger.butler.EnableSwaggerButler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @description:
+ * @author: tiger
+ * @date: 2018/9/14 20:36
+ * @version: V1.0
+ * @modified by:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableOAuth2Sso
+@EnableHystrix
+@EnableSwaggerButler
+public class TgcloudGatewayZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TgcloudGatewayZuulApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        // 允许cookies跨域
+        config.setAllowCredentials(true);
+        // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        config.addAllowedOrigin("*");
+        // #允许访问的头信息,*表示全部
+        config.addAllowedHeader("*");
+        // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+        config.setMaxAge(18000L);
+        // 允许提交请求的方法，*表示全部允许
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        // 允许Get的请求方法
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
